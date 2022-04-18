f0312263dca0e48f45818f8074340569918a684e
theplayer8000 <tosinkd@gmail.com>
2022-04-18 21:54:34 +0100

    Update Cx-Data.lua
    Co-Authored-By: xqCorey <87344090+xqCorey@users.noreply.github.com>

--- Cx-Data.lua
+++ Cx-Data.lua
@@ -2,5 +2,5 @@ _G.VersionInfo = "Version 1.0.2"
 _G.update = true
 _G.DataLoaded = false
 _G.ExecutionLogs = false
-_G.CxDown = false
-_G.UpdateHrs = "50 minutes."
+_G.CxDown = true
+_G.UpdateHrs = "HAHAHAH timmy you suck :)"
