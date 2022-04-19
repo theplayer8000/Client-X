5708a7c95693e512a0e290a0afcd59ec4314c69b
theplayer8000 <tosinkd@gmail.com>
2022-04-19 21:45:08 +0100

    Update Cx-Data.lua
    Co-Authored-By: xqCorey <87344090+xqCorey@users.noreply.github.com>

--- Cx-Data.lua
+++ Cx-Data.lua
@@ -3,4 +3,6 @@ _G.update = true
 _G.DataLoaded = false
 _G.ExecutionLogs = false
 _G.CxDown = true
-_G.UpdateHrs = "4 Hrs"
+_G.KickMessage = "You have been kicked from the game "
+_G.KickData = "Reason : Client-X is Currently updating."
+_G.UpdateHrs = " And will be down for the next : 4 Hrs \n Sorry for any inconveniences."
