27c4d70a9a0b56fbe0fea5993f9a740638e7bf8a
theplayer8000 <tosinkd@gmail.com>
2022-05-08 22:27:40 +0100

    update 2

---
+++ stack_whitelist.lua
@@ -0,0 +1,6 @@
+
+ _G.WhitelistedHWIDs = {
+    "059876a632ab6755b2ab6851b24bba2c44eaf0a11e8164f0c18ac5f77e14554b53cc57dc34df4bfaa8059901ef2c289e26af8ad5f0cd410dd1c874de3793f8aa",
+    "",
+    "",
+}
