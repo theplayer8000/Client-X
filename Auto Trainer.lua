27bb4a60dc6f69b56e66e87af5155c1d0b192b0c
theplayer8000 <76232039+theplayer8000@users.noreply.github.com>
2022-06-17 18:53:09 +0100

    Create Auto Trainer.lua

---
+++ Auto Trainer.lua
@@ -0,0 +1,66 @@
+if game.PlaceId ~= 2202352383 then return end
+while not game:IsLoaded() or not game:GetService("CoreGui") or
+    not game:GetService("Players").LocalPlayer or
+    not game:GetService("Players").LocalPlayer.PlayerGui do wait(1) end
+
+
+
+
+
+pc = #game.Players:GetPlayers()
+getgenv().autorj = true
+getgenv().autotrain = true
+pcn = 12
+if pc <= pcn then
+    print("smaller than or equal to 12")
+
+    getgenv().autorj = true
+else
+    getgenv().autorj = false
+end
+
+local cx = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/theplayer8000/Client-X/main/auto%20train.lua"))();'
+if syn.queue_on_teleport and getgenv().autorj == true then
+    syn.queue_on_teleport(cx)
+end
+
+
+    local COREGUI = game:GetService("CoreGui")
+local Players = game:GetService("Players")
+local Dir = COREGUI:FindFirstChild("RobloxPromptGui"):FindFirstChild("promptOverlay")
+local cx = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/theplayer8000/Client-X/main/auto%20train.lua"))();'
+
+Dir.DescendantAdded:Connect(
+    function(Err)
+        if Err.Name == "ErrorTitle" then
+            Err:GetPropertyChangedSignal("Text"):Connect(
+                function()
+                    if Err.Text:sub(0, 12) == "Disconnected" then
+                        if #Players:GetPlayers() <= 1 then
+                            Players.LocalPlayer:Kick("\nRejoining...")
+                            task.wait()
+                            game:GetService("GuiService"):ClearError()
+                            if syn.queue_on_teleport then
+                                syn.queue_on_teleport('game:GetService("ReplicatedFirst"):RemoveDefaultLoadingScreen()')
+                            end
+                            game:GetService("TeleportService"):Teleport(game.PlaceId, Players.LocalPlayer)
+                            if syn.queue_on_teleport and getgenv().autotrain == true then
+                                syn.queue_on_teleport(cx)
+                            end
+                        else
+                            game:GetService("GuiService"):ClearError()
+                            if syn.queue_on_teleport then
+                                syn.queue_on_teleport('game:GetService("ReplicatedFirst"):RemoveDefaultLoadingScreen()')
+                            end
+                            game:GetService("TeleportService"):Teleport(game.PlaceId, Players.LocalPlayer)
+                            if syn.queue_on_teleport and getgenv().autotrain == true then
+                                syn.queue_on_teleport(cx)
+                            end
+                        end
+                    end
+                end
+            )
+        end
+    end
+)
+
