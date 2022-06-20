08ebaf1f54c88de25a725d98bb574d7388849285
theplayer8000 <76232039+theplayer8000@users.noreply.github.com>
2022-06-20 18:58:14 +0100

    Update Auto Trainer.lua

--- Auto Trainer.lua
+++ Auto Trainer.lua
@@ -1,11 +1,13 @@
-if game.PlaceId ~= 2202352383 then return end
-while not game:IsLoaded() or not game:GetService("CoreGui") or
-    not game:GetService("Players").LocalPlayer or
-    not game:GetService("Players").LocalPlayer.PlayerGui do wait(1) end
-
+if game.PlaceId ~= 2202352383 then
+    return
+end
+while not game:IsLoaded() or not game:GetService("CoreGui") or not game:GetService("Players").LocalPlayer or
+    not game:GetService("Players").LocalPlayer.PlayerGui do
+    wait(1)
+end
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/theplayer8000/Client-X/main/serverHop.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/theplayer8000/Client-X/main/safety.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/theplayer8000/Client-X/main/safety.lua"))()
 
 pc = #game.Players:GetPlayers()
 getgenv().autorj = true
@@ -19,48 +21,56 @@ else
     getgenv().autorj = false
 end
 
-local cx = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/theplayer8000/Client-X/main/auto%20train.lua"))();'
+local cx =
+    'loadstring(game:HttpGet("https://raw.githubusercontent.com/theplayer8000/Client-X/main/auto%20train.lua"))();'
 if syn.queue_on_teleport and getgenv().autorj == true then
     syn.queue_on_teleport(cx)
 end
 
-
-    local COREGUI = game:GetService("CoreGui")
+local COREGUI = game:GetService("CoreGui")
 local Players = game:GetService("Players")
 local Dir = COREGUI:FindFirstChild("RobloxPromptGui"):FindFirstChild("promptOverlay")
-local cx = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/theplayer8000/Client-X/main/auto%20train.lua"))();'
+local cx =
+    'loadstring(game:HttpGet("https://raw.githubusercontent.com/theplayer8000/Client-X/main/auto%20train.lua"))();'
 
-Dir.DescendantAdded:Connect(
-    function(Err)
-        if Err.Name == "ErrorTitle" then
-            Err:GetPropertyChangedSignal("Text"):Connect(
-                function()
-                    if Err.Text:sub(0, 12) == "Disconnected" then
-                        if #Players:GetPlayers() <= 1 then
-                            Players.LocalPlayer:Kick("\nRejoining...")
-                            task.wait()
-                            game:GetService("GuiService"):ClearError()
-                            if syn.queue_on_teleport then
-                                syn.queue_on_teleport('game:GetService("ReplicatedFirst"):RemoveDefaultLoadingScreen()')
-                            end
-                            Teleport()
-                            if syn.queue_on_teleport and getgenv().autotrain == true then
-                                syn.queue_on_teleport(cx)
-                            end
-                        else
-                            game:GetService("GuiService"):ClearError()
-                            if syn.queue_on_teleport then
-                                syn.queue_on_teleport('game:GetService("ReplicatedFirst"):RemoveDefaultLoadingScreen()')
-                            end
-                            Teleport()
-                            if syn.queue_on_teleport and getgenv().autotrain == true then
-                                syn.queue_on_teleport(cx)
+while task.wait() do
+    if getgenv().autotrain == true then
+        Dir.DescendantAdded:Connect(
+            function(Err)
+                if Err.Name == "ErrorTitle" then
+                    Err:GetPropertyChangedSignal("Text"):Connect(
+                        function()
+                            if Err.Text:sub(0, 12) == "Disconnected" then
+                                if #Players:GetPlayers() <= 1 then
+                                    Players.LocalPlayer:Kick("\nRejoining...")
+                                    task.wait()
+                                    game:GetService("GuiService"):ClearError()
+                                    if syn.queue_on_teleport then
+                                        syn.queue_on_teleport(
+                                            'game:GetService("ReplicatedFirst"):RemoveDefaultLoadingScreen()'
+                                        )
+                                    end
+                                    Teleport()
+                                    if syn.queue_on_teleport and getgenv().autotrain == true then
+                                        syn.queue_on_teleport(cx)
+                                    end
+                                else
+                                    game:GetService("GuiService"):ClearError()
+                                    if syn.queue_on_teleport then
+                                        syn.queue_on_teleport(
+                                            'game:GetService("ReplicatedFirst"):RemoveDefaultLoadingScreen()'
+                                        )
+                                    end
+                                    Teleport()
+                                    if syn.queue_on_teleport and getgenv().autotrain == true then
+                                        syn.queue_on_teleport(cx)
+                                    end
+                                end
                             end
                         end
-                    end
+                    )
                 end
-            )
-        end
+            end
+        )
     end
-)
-
+end
